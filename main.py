4ca0a60c4ccd2013025869b8dd4da4e22982fb60
Alexander <principetolentinoa@hotmail.com>
2020-03-29 12:15:56 -0500

    Aumentos

--- main.py
+++ main.py
@@ -7,25 +7,28 @@ prev_casos = "0"
 aumentos = []
 def extract():
   global prev_casos
-  # Casos confirmados en Perú
-  #res = requests.get('https://www.worldometers.info/coronavirus/country/peru').text
+  # Casos confirmados de Coronavirus en Perú
+  # res = requests.get('https://www.worldometers.info/coronavirus/country/peru').text
 
   # Casos confirmados de Coronavirus en el mundo
   res = requests.get('https://www.worldometers.info/coronavirus/').text
   soup = BeautifulSoup(res, 'html.parser')
   soup.encode('utf-8')
-  casos = (soup.find("div", { "class": "maincounter-number" }).get_text().strip()).replace(',',"")
+  casos = (soup.find("div", { "class": "maincounter-number" }).get_text().strip())
+  casosint = int((soup.find("div", { "class": "maincounter-number" }).get_text().strip()).replace(',',""))
   
   if(casos == prev_casos):
-      print('['+ time.strftime('%l:%M:%S %p on %b %d, %Y') + ' ] SIN CAMBIOS EN EL NÚMERO DE CASOS')
+      print('['+ time.strftime('%l:%M:%S %p on %b %d, %Y') + ' ] SIN CAMBIOS EN EL NÚMERO DE CASOS (' + casos + ')')
 
   else:
     notifyMe('Total número de casos de CORONAVIRUS', casos)
     if(prev_casos != "0"):
+      prev_casosint = int(prev_casos.replace(',', ""))
       print('[' + time.strftime('%l:%M:%S %p on %b %d, %Y') + ' ] casos ROSE FROM ' + prev_casos + ' TO ' + casos)
-      notifyMe('El total de número de casos aumentó en {}'.format(int(float(casos)) - int(float(prev_casos))))
+      aumento = str(casosint - prev_casosint)
+      notifyMe('El total de número de casos aumentó en', aumento)
 
-      aumentos.append[int(float(casos)) - int(float(prev_casos))]
+      aumentos.append[aumento]
       print(aumentos)
     prev_casos = casos
 
